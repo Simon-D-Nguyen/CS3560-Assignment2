a3ec2c252a9c58e82a0cbdcab1c01c1e917c70f9
Simon Nguyen <simonnguyen99@gmail.com>
2024-06-28 20:04:33 -0700

    Added Comments to Observer and Subject

--- src/Users/Observer.java
+++ src/Users/Observer.java
@@ -1,5 +1,12 @@
 package Users;
 
 public interface Observer {
+
+    /**
+     * Updates the observer when called.
+     *
+     * @param update A string that represents the update/
+     *               update value.
+     */
     public void update(String update);
 }
--- src/Users/Subject.java
+++ src/Users/Subject.java
@@ -2,6 +2,17 @@ package Users;
 
 public interface Subject {
 
+    /**
+     * Attaches an observer to this subject to be
+     * updated when changes occur.
+     *
+     * @param observer observes change in subject.
+     */
     public void attach(Observer observer);
+
+
+    /**
+     * Notifies Observers when changes occur.
+     */
     public void notifyObservers();
 }
